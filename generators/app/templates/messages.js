befe97c3451b1fd95175ed66fdc7245c707b21a1
Tobias Skarhed <dehrax@users.noreply.github.com>
2016-09-21 11:10:00 +0200

    Update messages.js

--- generators/app/templates/messages.js
+++ generators/app/templates/messages.js
@@ -2,7 +2,7 @@ import {defineMessages} from 'react-intl';
 
 export default defineMessages({
 	placeholderMsg: {
-		id: "placeholder.message",
-		defaultMessage: "Message not yet implemnted"
+		id: 'placeholder.message',
+		defaultMessage: 'Message not yet implemnted'
 	}
 });
